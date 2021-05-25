f01c341bcfdcff88e2167fe602aacf3ac5f3d8ae
minji <swij1004@naver.com>
2021-05-26 01:37:52 +0900

    favicon, md 작성

---
+++ README.md
@@ -0,0 +1,29 @@
+## 📍 Project OverView 📍
+
+MERN stack의 basic concept 을 이해하고 활용하기 위해 진행한 프로젝트로,
+
+✨ 데모 링크 ✨ <br />
+
+### 사용 기술
+
+- HTML5 / CSS3
+- JavaScript(ES6+)
+- React
+- Node.js
+- Express
+- MongoDB, Mongoose
+- SCSS
+
+### 구현 기능
+
+- 아이템 검색
+- 전체 아이템 조회
+- 사용자 아이템 추가
+
+### ➕ 추가 예정
+
+- 모바일, 태블릿, 데스크탑 반응형 작업
+
+<br />
+<hr />
+<br />
--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
-import './App.css';
 import MainPage from './components/MainPage/MainPage';
 import ItemSearch from './components/ItemSearch/ItemSearch';
 import Template from './components/Template/Template';
 
-
 function App() {
-
   return(
     <Template>
       <MainPage />
       <ItemSearch />
     </Template>
   )
-
 }
 
 export default App;
