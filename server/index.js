3de2eb19cf4064af5b3c6c3751ef8ce83f3c7ce5
minji <swij1004@naver.com>
2021-06-03 02:55:03 +0900

    .

--- server/index.js
+++ server/index.js
@@ -22,11 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 
 app.use('/api', indexRouter);
 app.use('/api/admin', adminRouter);
