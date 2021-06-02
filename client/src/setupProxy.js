4e0c166e9c58c5f87559b1ae4528522b3b503afd
minji <swij1004@naver.com>
2021-06-03 02:22:06 +0900

    .

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/api/*',
       proxy({
         target:'http://localhost:5000',
         changeOrigin: true,
